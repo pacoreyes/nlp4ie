6b8dd7068ed2281eeb995a37445f9fcc70c3fbd1
Juan-Francisco Reyes <pacoreyesp@gmail.com>
2024-01-03 13:49:40 +0100

    paper c: reclassifying dataset 3 SetFit

--- paper_c_x_dl_chatgpt_classifier.py
+++ paper_c_x_dl_chatgpt_classifier.py
@@ -1,41 +1,112 @@
 from pprint import pprint
 
 from lib.utils import load_json_file, load_jsonl_file
+from lib.utils2 import anonymize_text
 
 import openai
+import spacy
 
-# The ID of your fine-tuned model
+WITH_ANONYMIZATION = True
+
+# The ID of your ChatGPT fine-tuned model
 model_id = "ft:davinci-002:paco::8cjtj5uI"  # anonymized
 # model_id = "ft:davinci-002:paco::8ck4FLNb"  # non-anonymized
 
 # Your API key from OpenAI
 openai.api_key = load_json_file("credentials/openai_credentials.json")["openai_api_key"]
 
-# dataset = read_from_google_sheet(spreadsheet_4, "dataset_3")
+# Load dataset
 dataset = load_jsonl_file("shared_data/dataset_3_3_openai.jsonl")
 
-pprint(dataset)
+# Load spaCy model
+nlp_trf = spacy.load("en_core_web_trf")
 
 
 def closest_to_zero(num1, num2):
   return min(num1, num2, key=abs)
 
 
-# The text you want to classify
-# text_to_classify = "I love the recent update, it made the law so much democratic and fairer for everyone"
-# text_to_classify = "I have doubts on the recent bill, it made the migrant's situation so much antidemocratic and unfair for everyone"
-# N/A
-text_to_classify = "And I've asked Louis Moret to serve in the Department of Energy specially director for the minority impact."
+support = [
+  "I want to explain why we have taken military action against Saddam Hussein, and why we believe this action is in the interests of the Iraqi people and all the people of the Middle East.",
+  "I'd like to see the day when every major river in our country is under control from its source to its mouth, when they are all wealth-givers instead of wealth-destroyers, when every one is running clean and pure and doing the work it ought to do for the people of this country.",
+  "Since small business has such a stabilizing influence on recession, I think this is a good sign for all Americans.",
+  "Our across-the-board cut in tax rates for a 3-year period will give them much of the incentive and promise of stability they need to go forward with expansion plans calling for additional employees.",
+  "It has the authority to administer a gas rationing plan, and prior to decontrol it ran the oil price control program.",
+  "And we will seek to cooperate with Russia to solve regional problems, while insisting that if Russian troops operate in neighboring states, they do so only when those states agree to their presence and in strict accord with international standards.",
+  "To overcome dangers in our world, we must also take the offensive by encouraging economic progress and fighting disease and spreading hope in hopeless lands.",
+  "These expenditure increases, let me stress, constitute practically all of the increases which have occurred under this administration, the remainder having gone to fight the recession we found in industry--mostly through the supplemental employment bill-and in agriculture."
+]
 
-# text_to_classify = "This can only mean the so-called normal of former reactionary administrations, the outstanding feature of which was a pittance for farm produce and a small wage for a long day of labor."
-# support
-# text_to_classify = "When that military objective is accomplished-and much of it has not yet been accomplished-the Italian people will be free to work out their own destiny, under a government of their own choosing."
-# oppose
-# text_to_classify = "We are more compassionate than a government that lets veterans sleep on our streets and families slide into poverty; that sits on its hands while a major American city drowns before our eyes."
+oppose = [
+  "His war against Iran cost at least half a million lives over 10 years.",
+  "They have fought against advances in housing and health and education.",
+  "At the same time, I think it cuts the defense budget too much, and by attempting to reduce the deficit through higher taxes, it will not create the kind of strong economic growth and the new jobs that we must have.",
+  "Adding to our troubles is a mass of regulations imposed on the shopkeeper, the farmer, the craftsman, professionals, and major industry that is estimated to add $100 billion to the price of the things we buy, and it reduces our ability to produce.",
+  "If we do nothing, American families will face a massive tax increase they do not expect and will not welcome.",
+  "It's because they understand that when I get a tax break I don't need and the country can't afford, it either adds to the deficit or somebody else has to make up the difference, like a senior on a fixed income or a student trying to get through school or a family trying to make ends meet.",
+  "Those with money and power will gain greater control over the decisions that could send a young soldier to war or allow another economic disaster or roll back the equal rights and voting rights that generations of Americans have fought, even died, to secure.",
+  "What I am concerned about is the kind of deficit we would have if we had a recession, and while the prospects for a recession are not certainly imminent before us, we do have to look at our historical record and realize that any society such as ours, particularly with the tax structure such as ours, must face that prospect at some time.",
+]
 
-res = openai.Completion.create(model=model_id, prompt=text_to_classify, max_tokens=1, temperature=0, logprobs=2)
+unclassified = [
+  "Members of this Congress who went there as observers told me of a woman who was wounded by rifle fire on the way to the polls, who refused to leave the line to have her wound treated until after she had voted.",
+  "Through the long years of conflict, four main issues have divided the parties involved.",
+  "Federal civilian employment, for example, is actually lower today than it was in 1952, while State and local government employment over the same period has increased 67 percent.",
+  "I hope that this lack will not make the veterans think I am any less deeply thrilled by the memory of their great comrades gone before - Grant, Hayes, Garfield, Harrison and McKinley - all sons of Ohio, who left records reflecting glory upon their State and Nation, or that my sympathies with the valor and courage and patriotism of those who faced death in the country's crises are any less earnest and sincere than they would be had I the right to wear a button of the Grand Army or of the veteran association of any of our country's wars.",
+  "We meet at one of those defining moments - a moment when our nation is at war, our economy is in turmoil, and the American promise has been threatened once more.",
+  "A little later he was made major general of Volunteers and placed in command of the Fifth Army Corps.",
+  "When it is remembered that before this terrific engagement Meade had been in command of the Army but three days, his victory becomes the more wonderful.",
+  "On the 1st of February, 1865, he was made a major general in the Regular Army."
+]
 
-pprint(res)
+print("################################")
+print("Support class:\n")
+
+for text in support:
+  if WITH_ANONYMIZATION:
+    text = anonymize_text(text, nlp_trf)
+
+  res = openai.Completion.create(model=model_id, prompt=text, max_tokens=1, temperature=0, logprobs=2)
+
+  pprint(res['choices'][0]["text"])
+  pprint(res['choices'][0]['logprobs']['top_logprobs'][0])
+  """oppose_prediction = res['choices'][0]['logprobs']['top_logprobs'][0]["op"]
+  support_prediction = res['choices'][0]['logprobs']['top_logprobs'][0]["support"]
+  print(f"Oppose: {oppose_prediction}")
+  print(f"Support: {support_prediction}")"""
+
+print("################################")
+print("Oppose class:\n")
+
+for text in oppose:
+  if WITH_ANONYMIZATION:
+    text = anonymize_text(text, nlp_trf)
+
+  res = openai.Completion.create(model=model_id, prompt=text, max_tokens=1, temperature=0, logprobs=2)
+
+  pprint(res['choices'][0]["text"])
+  pprint(res['choices'][0]['logprobs']['top_logprobs'][0])
+  """oppose_prediction = res['choices'][0]['logprobs']['top_logprobs'][0]["op"]
+  support_prediction = res['choices'][0]['logprobs']['top_logprobs'][0]["support"]
+  print(f"Oppose: {oppose_prediction}")
+  print(f"Support: {support_prediction}")"""
+
+print("################################")
+print("Unclassified class:\n")
+
+for text in unclassified:
+  if WITH_ANONYMIZATION:
+    text = anonymize_text(text, nlp_trf)
+
+  res = openai.Completion.create(model=model_id, prompt=text, max_tokens=1, temperature=0, logprobs=2)
+
+  pprint(res['choices'][0]["text"])
+  pprint(res['choices'][0]['logprobs']['top_logprobs'][0])
+  """oppose_prediction = res['choices'][0]['logprobs']['top_logprobs'][0]["op"]
+  support_prediction = res['choices'][0]['logprobs']['top_logprobs'][0]["support"]
+  print(f"Oppose: {oppose_prediction}")
+  print(f"Support: {support_prediction}")"""
+
 
 """oppose_prediction = res['choices'][0]['logprobs']['top_logprobs'][0]["op"]
 support_prediction = res['choices'][0]['logprobs']['top_logprobs'][0]["support"]
