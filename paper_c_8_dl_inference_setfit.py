f664cf539c39ce3b34c734e4b9cd494d8defcbc8
Juan-Francisco Reyes <pacoreyesp@gmail.com>
2024-01-24 14:59:03 +0100

    paper c: reclassifying dataset 3 SetFit

--- paper_c_8_dl_inference_setfit.py
+++ paper_c_8_dl_inference_setfit.py
@@ -5,7 +5,6 @@ import random
 import numpy as np
 import torch
 from setfit import SetFitModel
-from transformers import AutoTokenizer
 
 from db import spreadsheet_4
 from lib.utils import load_jsonl_file, read_from_google_sheet
@@ -21,8 +20,7 @@ model_setfit_path = "models/3"
 model = SetFitModel.from_pretrained(model_setfit_path, local_files_only=True)
 # Set model id
 model_id = "sentence-transformers/paraphrase-mpnet-base-v2"
-# Load tokenizer
-tokenizer = AutoTokenizer.from_pretrained(model_id)
+
 # Load training and validation datasets
 dataset_training = load_jsonl_file("shared_data/dataset_3_4_training_anonym.jsonl")
 dataset_validation = load_jsonl_file("shared_data/dataset_3_5_validation_anonym.jsonl")
@@ -74,15 +72,14 @@ if preds.is_cuda:
 
 predictions_list = preds.numpy().tolist()
 
-# validity_threshold = 0.9925
-
+# Filter predictions by class and score using thresholds
 for idx, p in enumerate(predictions_list):
   pred_class = None
   pred_score = 0
-  if p[0] > p[1] and p[0] > 0.9945 and p[0] < 0.9947:  # 0.9931
+  if p[0] > p[1] and p[0] > 0.9945:  # 0.9931
     pred_class = "support"
     pred_score = p[0]
-  elif p[0] < p[1] and p[1] > 0.9945 and p[0] < 0.9947:  # 0.9930
+  elif p[0] < p[1] and p[1] > 0.9948:  # 0.9930
     pred_class = "oppose"
     pred_score = p[1]
   else:
